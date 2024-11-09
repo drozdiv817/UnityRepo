4508e8bc998cd7c8ec38b2878bbf6da57010053b
drozdiv817 <drozdivan2802@gmail.com>
2024-11-09 18:36:55 +0300

    Add files via upload

---
+++ NewBehaviourScript.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    
+    // // We dont need the start in this script, so we can remove in :)
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Prints "crickets" every frame
+        print("crickets\n");
+    }
+}
